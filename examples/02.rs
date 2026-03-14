f92ef0e690837381716f1a515737c7ebebcc37be
Vincent <vincent.devilers@gmail.com>
2026-03-14 18:26:12 +0100

    learning(example-02): first implementation of a basic app state
    this countains a counter that will be used later on and an exit value
    that we will set to true whenever we want to kill this app / exit

--- examples/02.rs
+++ examples/02.rs
@@ -6,19 +6,30 @@ use ratatui::{DefaultTerminal, widgets::Paragraph};
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
-    ratatui::run(app);
+    let _ = ratatui::run(app);
     Ok(())
 }
 
 fn app(terminal: &mut DefaultTerminal) -> io::Result<()> {
-    loop {
-        terminal.draw(|frame| {
-            let hello = format!("Hello world");
-            let para = Paragraph::new(hello);
-            frame.render_widget(para, frame.area());
-        })?;
-        if crossterm::event::read()?.is_key_press() {
-            break Ok(());
+    App::default().run(terminal)
+}
+
+#[derive(Debug, Default)]
+struct App {
+    counter: u8,
+    exit: bool,
+}
+
+impl App {
+    pub fn run(&mut self, terminal: &mut DefaultTerminal) -> io::Result<()> {
+        while !self.exit {
+            terminal.draw(|frame| {
+                frame.render_widget(Paragraph::new("Hello from the other side"), frame.area());
+            })?;
+            if crossterm::event::read()?.is_key_press() {
+                self.exit = true;
+            }
         }
+        Ok(())
     }
 }
