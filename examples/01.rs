1909f4f526df8c9c2177ad02d704b0fa8bd527d3
Vincent <vincent.devilers@gmail.com>
2026-03-14 16:12:23 +0100

    learning(example-01): messing with Event type their related enums

--- examples/01.rs
+++ examples/01.rs
@@ -1,6 +1,6 @@
 use std::io;
 
-use crossterm::event::{self, KeyCode, KeyEventKind};
+use crossterm::event::{self, Event, KeyCode, KeyEvent, KeyEventKind};
 use ratatui::{DefaultTerminal, style::Stylize, widgets::Paragraph};
 
 #[tokio::main]
@@ -18,13 +18,54 @@ fn run(mut terminal: DefaultTerminal) -> io::Result<()> {
     loop {
         c += 1;
         terminal.draw(|frame| {
-            let greeting = Paragraph::new(format!("Hello world")).red();
+            let greeting = Paragraph::new(format!("Hello world"))
+                .red()
+                .on_green()
+                .bold();
             frame.render_widget(greeting, frame.area());
         })?;
-        if let event::Event::Key(key) = event::read()? {
-            if key.kind == KeyEventKind::Press && key.code == KeyCode::Char('q') {
+        if let Ok(action) = handle_events() {
+            if action == Actionlist::Quit {
                 return Ok(());
             }
+        }
+    }
+}
+
+fn handle_events() -> std::io::Result<Actionlist> {
+    let action = match event::read()? {
+        Event::Key(key) => handle_key_event(&key),
+        _ => Actionlist::Nothing,
+    };
+
+    Ok(action)
+}
+
+fn handle_key_event(event: &KeyEvent) -> Actionlist {
+    match event.kind {
+        KeyEventKind::Press => handle_key_code(&event.code),
+        _ => Actionlist::Nothing,
+    }
+}
+
+fn handle_key_code(key_code: &KeyCode) -> Actionlist {
+    match key_code {
+        KeyCode::Char('q') => Actionlist::Quit,
+        _ => Actionlist::Nothing,
+    }
+}
+
+#[derive(Debug, PartialEq, Eq)]
+enum Actionlist {
+    Quit,
+    Nothing,
+}
+
+impl Actionlist {
+    pub fn handle(&self) {
+        match self {
+            Self::Quit => (),
+            Self::Nothing => (),
         }
     }
 }
