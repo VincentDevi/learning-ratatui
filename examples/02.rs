462237ac81617736081fd2022cd50cb4cc1aa482
Vincent <vincent.devilers@gmail.com>
2026-03-15 13:26:52 +0100

    learning(example-02): first counter
    Without any proper UI, create a counter that increment whenever we press
    up and decrement whenever we press down ( left and right also works ).
    
    error handling is not porperly done, we should use colo_eyre whenever
    the app crashes.
    UI still needs to be created ( would be good to enable some mouse
    action)

--- examples/02.rs
+++ examples/02.rs
@@ -1,5 +1,6 @@
 use std::{error::Error, io};
 
+use crossterm::event::{Event, KeyCode, KeyEvent, KeyEventKind};
 use ratatui::{DefaultTerminal, widgets::Paragraph};
 
 // Ratatui first interactive application
@@ -24,12 +25,55 @@ impl App {
     pub fn run(&mut self, terminal: &mut DefaultTerminal) -> io::Result<()> {
         while !self.exit {
             terminal.draw(|frame| {
-                frame.render_widget(Paragraph::new("Hello from the other side"), frame.area());
+                frame.render_widget(
+                    Paragraph::new(format!(" Counter : {}", self.counter)),
+                    frame.area(),
+                );
             })?;
-            if crossterm::event::read()?.is_key_press() {
-                self.exit = true;
-            }
+            let _ = self.handle_key_events();
         }
         Ok(())
     }
+
+    fn handle_key_events(&mut self) -> io::Result<()> {
+        if let Event::Key(event_key) = crossterm::event::read()? {
+            let app_action = match event_key.kind {
+                KeyEventKind::Press => match event_key.code {
+                    KeyCode::Char('q') | KeyCode::Char('Q') => AppAction::Close,
+                    KeyCode::Up | KeyCode::Right => AppAction::Increment,
+                    KeyCode::Down | KeyCode::Left => AppAction::Decrement,
+                    _ => AppAction::Nothing,
+                },
+                _ => AppAction::Nothing,
+            };
+            self.handle_app_action(&app_action);
+        };
+        Ok(())
+    }
+
+    fn handle_app_action(&mut self, app_action: &AppAction) {
+        match app_action {
+            AppAction::Close => self.close(),
+            AppAction::Decrement => self.decrement(),
+            AppAction::Increment => self.increment(),
+            AppAction::Nothing => (),
+        }
+    }
+    fn increment(&mut self) {
+        let _ = self.counter = self.counter + 1;
+    }
+    fn decrement(&mut self) {
+        let _ = self.counter = self.counter - 1;
+    }
+    fn close(&mut self) {
+        self.exit = true;
+    }
+}
+
+#[derive(Debug)]
+enum AppAction {
+    Close,
+    Increment,
+    Decrement,
+    Nothing,
 }
